b9fb983ce8f7abccf798315e989479518a64f613
Michael Folger <folger@zoller-usa.com>
2021-07-06 23:58:22 -0400

    updates to html, js

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ setInterval(displayTime, 1000);
 
 var buttonEl = $('#search-btn');
 
+//API Key for Weather API Call
+var APIKey = "0dc6bffb22b00026825caac91f732003";
+
+//User input variables
+var city = "";
+var state ="";
+
+
+//save user input to local storage
+var userInput = document.getElementById(userCityInput);
+
+//store
+localStorage.setItem("city", userInput);
+
+//Load item and show in console
+console.log(localStorage.getItem('city'))
+
+//save user input into a variable
+function cityName() {
+  var userInputCity = document.getElementById('userInput-city')
+  console.log(userInputCity);
+};
+
+cityName();
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+//API Call
+//fetch (queryURL);
